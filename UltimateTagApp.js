35c1a052171c59dcf78601af1362d6cec88ede63
Quint Fain <quintfain@Quints-MacBook-Pro.local>
2019-04-18 14:00:20 -0700

    Class inittialization.

--- UltimateTagApp.js
+++ UltimateTagApp.js
@@ -1,31 +1,97 @@
 class UltimateTagApp {
     constructor() {
+        this.player1 = new Player();
+        this.player2 = new Player();
+        this.Scoreboard = new Scoreboard();
 
     }
 
+    movePlayer(){
 
+    }
+
+    assignRoles(){
+
+    }
 }
+
+
+
 
 class Player {
-    constructor() {
-
+    constructor(_xpos, _ypos, _color, _it) {
+        this.xpos = _xpos;
+        this.ypos = _ypos;
+        this.color = _color;
+        this.it = _it;
     }
 }
+
+
+
+
 
 class Scoreboard {
     constructor() {
+        this.timer = new Timer();
+        this.score = 0;
+    }
+
+    updateScore(){
 
     }
 }
+
+
+
+
 
 class Obstacles {
-    constructor() {
+    constructor(_color) {
+        this.color = _color;
+    }
+
+    slowPlayer(){
 
     }
+
+    fasterEnemies(){
+
+    }
+
 }
+
+
+
+
 
 class PowerUps() {
     constructor() {
+        this.color = "green";
+    }
+
+    moreSpeed(){
+
+    }
+
+    invisibility(){
+
+    }
+}
+
+
+
+
+class Timer(){
+    constructor(){
+        this.time = 0;
+    }
+
+    upDateTime(){
+
+    }
+
+    getTime(){
 
     }
 }
